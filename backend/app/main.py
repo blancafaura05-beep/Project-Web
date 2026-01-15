b3b9ed71368bc58d25eed067b996d3f358fb6443
blancafaura05-beep <blancafaura05@gmail.com>
2026-01-15 19:28:22 +0100

    sincronizar

--- backend/app/main.py
+++ backend/app/main.py
@@ -22,6 +22,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
+        "https://project-web-frontend-olive.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
